d7d609a79632a0d90216ac2ace44d21b134047c8
郁世昌 <yushichang0845@dingtalk.com>
2019-03-20 15:32:15 +0800

    项目搭建

---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+
+module.exports = {
+  entry: './src/app.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',  // 动态引入
+    filename: 'js/app.js'
+  },
+  devServer: {
+    port: 8081,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192, // 8k
+              name: 'static/[name].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192, // 8k
+              name: 'static/[name].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    // 处理html文件
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+    // 独立css文件
+    new ExtractTextPlugin('css/[name].css'),
+    //提出公共模块
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'common',
+      filename: 'js/base.js'
+    })
+  ]
+};
